8902b0e75a12d9356dcc093b19ae1ed21cb2dcfa
Andrzej Borucki <borneq@example.com>
2013-09-18 22:38:51 +0200

    first approach to controls, tables

--- src/com/borneq/as/Commander.java
+++ src/com/borneq/as/Commander.java
@@ -1,18 +1,128 @@
 package com.borneq.as;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
+import java.awt.LayoutManager;
+import java.awt.event.KeyEvent;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+
+import com.borneq.awt.BordListLayout;
+
+import java.awt.event.ActionEvent;
 
 public class Commander extends JFrame {
+	private JMenuBar menuBar;
+	private JMenu menu, submenu;
+	private JMenuItem menuItem;
+	private JRadioButtonMenuItem rbMenuItem;
+	private JCheckBoxMenuItem cbMenuItem;
+	private JPanel fkeyPanel;
+	private JPanel commandPanel;
+	private JTable tableLeft, tableRight;
+	private JScrollPane scrollLeft, scrollRight;
+	private FileModel model;
 
 	public Commander(String title) {
 		super(title);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initMenu();
+		LayoutManager layout = new BordListLayout();
+		setLayout(layout);
+		fkeyPanel = new JPanel();
+		fkeyPanel.add(new JButton("F3 View"));
+		fkeyPanel.add(new JButton("F4 Edit"));
+		fkeyPanel.add(new JButton("F5 Copy"));
+		fkeyPanel.add(new JButton("F6 Move"));
+		fkeyPanel.add(new JButton("F7 NewFolder"));
+		fkeyPanel.add(new JButton("F8 Delete"));
+		fkeyPanel.add(new JButton("Alt+F4 Exit"));
+
+		commandPanel = new JPanel();
+		commandPanel.add(new JLabel());
+		commandPanel.add(new JTextField(20));
+
+		add(fkeyPanel, BordListLayout.SOUTH);
+		add(commandPanel, BordListLayout.SOUTH);
+
+		tableLeft = new JTable(model);
+		tableRight = new JTable(model);
+		scrollLeft = new JScrollPane(tableLeft);
+		scrollRight = new JScrollPane(tableRight);
+		add(scrollLeft, BordListLayout.CENTER);
+		add(scrollRight, BordListLayout.CENTER);
 	}
+
+	private void initMenu() {
+		menuBar = new JMenuBar();
+		menu = new JMenu("A Menu");
+		menu.setMnemonic(KeyEvent.VK_A);
+		menu.getAccessibleContext().setAccessibleDescription(
+				"The only menu in this program that has menu items");
+		menuBar.add(menu);
+
+		// a group of JMenuItems
+		menuItem = new JMenuItem("A text-only menu item", KeyEvent.VK_T);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1,
+				ActionEvent.ALT_MASK));
+		menuItem.getAccessibleContext().setAccessibleDescription(
+				"This doesn't really do anything");
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem("Both text and icon", new ImageIcon(
+				"images/middle.gif"));
+		menuItem.setMnemonic(KeyEvent.VK_B);
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+		menuItem.setMnemonic(KeyEvent.VK_D);
+		menu.add(menuItem);
+
+		// a group of radio button menu items
+		menu.addSeparator();
+		ButtonGroup group = new ButtonGroup();
+		rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+		rbMenuItem.setSelected(true);
+		rbMenuItem.setMnemonic(KeyEvent.VK_R);
+		group.add(rbMenuItem);
+		menu.add(rbMenuItem);
+
+		rbMenuItem = new JRadioButtonMenuItem("Another one");
+		rbMenuItem.setMnemonic(KeyEvent.VK_O);
+		group.add(rbMenuItem);
+		menu.add(rbMenuItem);
+
+		// a group of check box menu items
+		menu.addSeparator();
+		cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+		cbMenuItem.setMnemonic(KeyEvent.VK_C);
+		menu.add(cbMenuItem);
+
+		cbMenuItem = new JCheckBoxMenuItem("Another one");
+		cbMenuItem.setMnemonic(KeyEvent.VK_H);
+		menu.add(cbMenuItem);
+
+		// a submenu
+		menu.addSeparator();
+		submenu = new JMenu("A submenu");
+		submenu.setMnemonic(KeyEvent.VK_S);
+
+		menuItem = new JMenuItem("An item in the submenu");
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2,
+				ActionEvent.ALT_MASK));
+		submenu.add(menuItem);
+
+		menuItem = new JMenuItem("Another item");
+		submenu.add(menuItem);
+		menu.add(submenu);
+
+		// Build second menu in the menu bar.
+		menu = new JMenu("Another Menu");
+		menu.setMnemonic(KeyEvent.VK_N);
+		menu.getAccessibleContext().setAccessibleDescription(
+				"This menu does nothing");
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
+	}
+
 	protected static void createAndShowGUI() {
 		final Commander frame = new Commander("As Commander");
 		frame.pack();
@@ -24,6 +134,7 @@ public class Commander extends JFrame {
 			}
 		});
 	}
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
---
+++ src/com/borneq/as/FileModel.java
@@ -0,0 +1,25 @@
+package com.borneq.as;
+
+import javax.swing.table.AbstractTableModel;
+
+public class FileModel extends AbstractTableModel {
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
